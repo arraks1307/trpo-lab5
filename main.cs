6f5d181094aa5891ec32f9f5bb65b35e6ca0e840
Danila <arraks1307@mail.ru>
2026-04-28 20:27:31 +0300

    Готовая лабораторная работа №5: добавлен метод IsPrime

--- main.cs
+++ main.cs
@@ -5,5 +5,26 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello, World!");
+        // Пример использования
+        Console.WriteLine(IsPrime(7));  // True
+        Console.WriteLine(IsPrime(10)); // False
+        Console.WriteLine(IsPrime(2));  // True
+        Console.WriteLine(IsPrime(1));  // False
+    }
+    
+    // функция которое проверяет простое число или нет
+
+    static bool IsPrime(int n)
+    {
+        if (n <= 1)
+            return false;
+        
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
     }
 }
+
